bb0bb1f1a9307fbe546b7178bac742b5b205b1e1
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-17 14:05:26 +0000

    created positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d is POSITIVE\n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is NEGATIVE\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is ZERO\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
