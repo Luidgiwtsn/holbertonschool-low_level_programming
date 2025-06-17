7eb8a516bc202fb6b7fdb421b96f6c8040c22403
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-17 09:51:05 +0000

    created printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**                                                                                                                                                                                                                                
- * main - function                                                                                                                                                                                                                 
- *                                                                                                                                                                                                                                 
- * Return: Always 0 (Success)                                                                                                                                                                                                      
+/**
+ * main - function
+ *
+ * Return: Always 0 (Success)
 **/
 int main(void)
 {
