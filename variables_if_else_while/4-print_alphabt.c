7bd58ab04c81b556707b531910d010d1b793a927
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-18 13:53:08 +0000

    created print alphabet.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/** 
+*main - function prints alphabetly*
+*Return: Always 0.
+*/
+
+int main(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+
+if (n != 'e' && n != 'q')
+
+putchar(n);
+
+putchar('\n');
+
+return (0);
+}
