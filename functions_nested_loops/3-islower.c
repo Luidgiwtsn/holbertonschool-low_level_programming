449ee23e3432b0c071795038d5d9648fac92579f
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-20 11:34:27 +0000

    created islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-*_islower-function checks c islower or not
-*
-*the charscters is input
-* Return: 1 for lowercase. 0 for the rest.
-*/
+ * _islower - Entry point
+ *
+ * @c: variable
+ *
+ * Return: 0 - upper OR 1 - lower
+ */
 
 int _islower(int c)
 {
