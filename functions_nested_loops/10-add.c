266d1d522be748dc77c8fcdc0f30fd9ac300c927
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-20 12:45:15 +0000

    created add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add -function add two integers
+*@a:first params
+*@b:seconds params
+*main - check the code
+*Return: Always 0.
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
