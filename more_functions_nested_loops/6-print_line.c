d40ff089cecae387467d8cb62da19c4e6aed6c46
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-24 12:30:36 +0000

    created print lines.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_line - Write a function that draws a straight line in the terminal.
+*@n: Integer amount of _
+*/
+
+void print_line(int n)
+{
+int a;
+ if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
