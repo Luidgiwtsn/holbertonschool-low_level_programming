e19e6e938e29dc6e58a4e330a7f6b422f0ee1e14
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-27 10:09:17 +0000

    created puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
