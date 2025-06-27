4e0afede6eedb89664b8d62c656ef2ccff596350
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-27 13:27:08 +0000

    print rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reverse_array - function returns reverse of array
+* @a: takes in a character
+* @n:take in a char
+* Return: void
+*/
+
+void print_rev(char *s);
