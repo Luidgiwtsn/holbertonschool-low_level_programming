91e17acc2e7089d5b1a402a517ea555a36555086
Luidgiwtsn <lwbordeaux@gmail.com>
2025-06-27 12:40:06 +0000

    created puts.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n);
+int _putchar(char c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
