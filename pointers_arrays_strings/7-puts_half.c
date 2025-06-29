e186eb2da807e468a720425276e90eef2b01a54f
luidgiwtsn <lwbordeaux@gmail.com>
2025-06-29 06:37:49 +0000

    created puts half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,26 +8,30 @@
  *
  * Return: Always 0 (Success)
  */
+
 void puts_half(char *str)
 {
 int c = 0;
 int i;
 
-c = strlen(str);
+while (str[c] != '\0')
+{
+     c++;
+}
 
 if (c % 2 == 0)
-	{
+{
 for (i = c / 2; i < c; i++)
-		{
+{
 _putchar(str[i]);
-		}
-	}
+}
+}
 else
-	{
+{
 for (i = (c + 1) / 2; i < c; i++)
-		{
+{
 _putchar(str[i]);
-		}
-	}
+}
+}
 _putchar('\n');
 }
