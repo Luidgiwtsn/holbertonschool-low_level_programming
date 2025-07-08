6dee520e3a8416eb401a9cb11ff574d6b074c272
luidgiwtsn <lwbordeaux@gmail.com>
2025-07-08 11:41:18 +0000

    created alloc grid.c

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -46,4 +46,5 @@ void _print_rev_recursion(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif
