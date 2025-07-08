ff3e746a394a5071107ea59cca896fabb70b48ab
luidgiwtsn <lwbordeaux@gmail.com>
2025-07-08 08:35:23 +0000

    created str concat.c

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -45,4 +45,5 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
