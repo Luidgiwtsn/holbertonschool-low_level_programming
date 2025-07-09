e35603a72e7e290750ac3523d89a6b1412ef4979
Luidgiwtsn <lwbordeaux@gmail.com>
2025-07-09 07:57:59 +0000

    created malloc checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: variable to allocate memory for
+ * Return: Pointer to the allocated memory. Exits with status 98 if malloc fails.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *memory = malloc(b);
+
+if (memory == NULL)
+exit(98);
+
+return memory;
+}
